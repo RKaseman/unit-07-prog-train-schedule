4e16b2be01e2a3997c7a8646d1b5838402fb0899
Rane M Kaseman <raykman@comcast.net>
2018-08-13 18:00:31 -0600

    1800

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+    var trName;
+    var trDest;
+    var trArrive
+    var trLeave
+    var trFreq;
+
+$("#sched").append("<div><tr><td></td><td></td><td></td><td></td><td></td></div>")
+
+});
+
