23d535ce43d678896ef1210e3c504ad919522bc1
Rane M Kaseman <raykman@comcast.net>
2018-08-14 18:36:25 -0600

    1836

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,40 @@
 
-$(document).ready(function () {
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBmeleQU0z3P1C6sbEnlStaE7msi-2JzxM",
+    authDomain: "trains-35781.firebaseapp.com",
+    databaseURL: "https://trains-35781.firebaseio.com",
+    projectId: "trains-35781",
+    storageBucket: "",
+    messagingSenderId: "755160834451"
+};
 
-    var trName;
-    var trDest;
-    var trArrive
-    var trLeave
-    var trFreq;
+firebase.initializeApp(config);
 
-$("#sched").append("<div><tr><td></td><td></td><td></td><td></td><td></td></div>")
+var fbDb = firebase.database();
+
+$("#fullForm").on("click", function(event){
+    event.PreventDefault();
+    var name = $("#nameEnter").val().trim();
+    var destination = $("#destEnter").val().trim();
+    var arrival = $("#arrivalEnter").val().trim();
+    var departure = $("#departEnter").val().trim();
+    var frequency = $("#freqEnter").val().trim();
+
+    console.log(name);
+
+    // fbDb.ref().push({
+    //     name: name,
+    //     destination: destination,
+    //     arrival: arrival,
+    //     departure: departure,
+    //     frequency: frequency,
+    // })
 
 });
+
+// fbDb.ref().on("child_added", function(childSs){
+//     console.log(childSs.val());
+//     $("#tbody").append("<tr><td>" + childSs.val().name + "</td><td>" + childSs.val().destination + "</td><td>" + childSs.val().arrival + "</td><td>" + childSs.val().departure + "</td><td>" + childSs.val().frequency + "</td></tr>");
+// });
 
