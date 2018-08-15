f346292f96b6ce8d302ffde4b02a40d3188df16a
Rane M Kaseman <raykman@comcast.net>
2018-08-14 23:35:28 -0600

    2335

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -5,36 +5,48 @@ var config = {
     authDomain: "trains-35781.firebaseapp.com",
     databaseURL: "https://trains-35781.firebaseio.com",
     projectId: "trains-35781",
-    storageBucket: "",
+    storageBucket: "trains-35781.appspot.com",
     messagingSenderId: "755160834451"
 };
 
 firebase.initializeApp(config);
 
-var fbDb = firebase.database();
+var name = "";
+var destination = "";
+var arrival = "";
+var departure = "";
+var frequency = "";
 
-$("#fullForm").on("click", function(event){
-    event.PreventDefault();
-    var name = $("#nameEnter").val().trim();
-    var destination = $("#destEnter").val().trim();
-    var arrival = $("#arrivalEnter").val().trim();
-    var departure = $("#departEnter").val().trim();
-    var frequency = $("#freqEnter").val().trim();
-
-    console.log(name);
-
-    // fbDb.ref().push({
-    //     name: name,
-    //     destination: destination,
-    //     arrival: arrival,
-    //     departure: departure,
-    //     frequency: frequency,
-    // })
-
+firebase.database().ref().set({
+    name: name,
+    destination: destination,
+    arrival: arrival,
+    departure: departure,
+    frequency: frequency,
 });
+
+$("#sendForm").on("click", function(event){
+//     event.PreventDefault();
+    name = $("#nameEnter").val().trim();
+    destination = $("#destEnter").val().trim();
+    arrival = $("#arrivalEnter").val().trim();
+    departure = $("#departEnter").val().trim();
+    frequency = $("#freqEnter").val().trim();
+
+//     console.log(name);
+
+//     // fbDb.ref().push({
+//     //     name: name,
+//     //     destination: destination,
+//     //     arrival: arrival,
+//     //     departure: departure,
+//     //     frequency: frequency,
+//     // })
+
+// });
 
 // fbDb.ref().on("child_added", function(childSs){
 //     console.log(childSs.val());
 //     $("#tbody").append("<tr><td>" + childSs.val().name + "</td><td>" + childSs.val().destination + "</td><td>" + childSs.val().arrival + "</td><td>" + childSs.val().departure + "</td><td>" + childSs.val().frequency + "</td></tr>");
-// });
+});
 
